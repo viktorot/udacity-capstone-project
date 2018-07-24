b3c931a40cc0e068abe283b14b72a34d6cb62836
Viktor <viktor@Deniss-iMac.local>
2018-07-24 10:59:32 +0200

    - note list item updates

--- notefy/app/src/main/java/io/viktorot/notefy/ui/list/NoteListViewItem.java
+++ notefy/app/src/main/java/io/viktorot/notefy/ui/list/NoteListViewItem.java
@@ -2,6 +2,7 @@ package io.viktorot.notefy.ui.list;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,21 +10,29 @@ import android.widget.TextView;
 import com.xwray.groupie.Item;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import io.viktorot.notefy.NotefyApplication;
 import io.viktorot.notefy.R;
 import io.viktorot.notefy.data.Note;
+import io.viktorot.notefy.repo.IconRepo;
 import io.viktorot.notefy.repo.TagRepo;
 import io.viktorot.notefy.util.ViewUtils;
 
 class NoteListViewItem extends Item<NoteListViewItem.ViewHolder> {
 
+    private final Context context;
+
     private final Note data;
 
     private final TagRepo tagRepo;
+    private final IconRepo iconRepo;
 
     NoteListViewItem(@NonNull Context context, @NonNull Note data) {
+        this.context = context;
         this.data = data;
+
         this.tagRepo = NotefyApplication.get(context).getTagRepo();
+        this.iconRepo = NotefyApplication.get(context).getIconRepo();
     }
 
     @NonNull
@@ -46,8 +55,19 @@ class NoteListViewItem extends Item<NoteListViewItem.ViewHolder> {
         if (tagRepo.isIdValid(id)) {
             viewHolder.tvTag.setText(tagRepo.getTag(id));
             ViewUtils.show(viewHolder.tvTag);
+            ViewUtils.show(viewHolder.imgTag);
         } else {
             ViewUtils.hide(viewHolder.tvTag);
+            ViewUtils.hide(viewHolder.imgTag);
+        }
+
+        Drawable icon = ContextCompat.getDrawable(context, iconRepo.getIconRes(data.getIconId()));
+        viewHolder.imgIcon.setImageDrawable(icon);
+
+        if (this.data.isPinned()) {
+            ViewUtils.show(viewHolder.imgPin);
+        } else {
+            ViewUtils.hide(viewHolder.imgPin);
         }
 
         viewHolder.imgCorner.setColorFilter(Color.parseColor(this.data.getColor()));
@@ -62,14 +82,20 @@ class NoteListViewItem extends Item<NoteListViewItem.ViewHolder> {
         final TextView tvTitle;
         final TextView tvContent;
         final TextView tvTag;
+        final ImageView imgTag;
         final ImageView imgCorner;
+        final ImageView imgPin;
+        final ImageView imgIcon;
 
         ViewHolder(@NonNull View rootView) {
             super(rootView);
             tvTitle = rootView.findViewById(R.id.title);
             tvContent = rootView.findViewById(R.id.content);
-            tvTag = rootView.findViewById(R.id.tag);
+            tvTag = rootView.findViewById(R.id.tag_text);
+            imgTag = rootView.findViewById(R.id.tag_img);
             imgCorner = rootView.findViewById(R.id.corner);
+            imgPin = rootView.findViewById(R.id.pin);
+            imgIcon = rootView.findViewById(R.id.icon);
         }
     }
 }
